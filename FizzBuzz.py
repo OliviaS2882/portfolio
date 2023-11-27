dc4183888fbacfe4a89f15c31580b2aa4adea6ff
OliviaS2882 <151667011+OliviaS2882@users.noreply.github.com>
2023-11-27 15:09:13 +0000

    Add files via upload

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,8 @@
 for number in range(1, 101):
   if number % 3 == 0 and number % 5 == 0:
-    number = "FizzBuzz"
+    print("FizzBuzz")
   elif number % 3 == 0:
-    number = "Fizz"
+    print("Fizz")
   elif number % 5 == 0:
-    number = "Buzz"
+    print("Buzz")
   print(number)
