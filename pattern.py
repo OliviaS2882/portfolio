39e5ab8b7ae36606225e2f719beb73f85d7e50ad
OliviaS2882 <151667011+OliviaS2882@users.noreply.github.com>
2023-12-03 11:06:02 +0000

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,17 @@
+  
+for i in range(1, 10):          #Resulting pattern has 9 iterations.
+    if i <= 5:                  #For first five iterations, pattern will increase. 
+        print('*' * i)
+    else:
+        print('*' * (10 - i))   #For iterations 6-9 the pattern will decrease.
+
+
+
+
+
+        
+        
+
+
+  
+        
